f98793e88ddb99af7bd34cde003f28531eeb5617
Siddharth Dhanraj@d1_49619 <siddharthdhanraj90@gmail.com>
2025-03-01 09:31:33 +0530

    Added Changes for Staging

---
+++ az-app-sid-vs/Pages/DeploymentSlots.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace az_app_sid_vs.Pages
+{
+    public class DeploymentSlotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
