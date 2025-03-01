b462a3d8b1e2a21e869fdff50a0f3072a4d314f5
Siddharth Dhanraj@d1_49619 <siddharthdhanraj90@gmail.com>
2025-03-01 09:04:57 +0530

    Added Configuration Settings for ViewData

--- az-app-sid-vs/Pages/Privacy.cshtml.cs
+++ az-app-sid-vs/Pages/Privacy.cshtml.cs
@@ -6,14 +6,18 @@ namespace az_app_sid_vs.Pages;
 public class PrivacyModel : PageModel
 {
     private readonly ILogger<PrivacyModel> _logger;
+    private readonly IConfiguration _configuration;
 
-    public PrivacyModel(ILogger<PrivacyModel> logger)
+    public PrivacyModel(ILogger<PrivacyModel> logger,IConfiguration configuration)
     {
         _logger = logger;
+        this._configuration = configuration;
     }
 
     public void OnGet()
     {
+          
+       
     }
 }
 
